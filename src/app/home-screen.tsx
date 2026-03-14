abb113380d0249f1c5013582b5fa06c9402af927
krishnaxcodergit config --global user.email krishnaxcoder@gmail.comgit config --global user.name krishnaxcoder <krishnaxcoder@gmail.com>
2026-03-14 19:27:51 +0530

    asd

--- src/app/home-screen.tsx
+++ src/app/home-screen.tsx
@@ -1,8 +1,44 @@
 "use client";
 
+import { Component1 } from "@/app/components/component1";
+import { Component3 } from "@/app/components/component3";
+import { Component4 } from "@/app/components/component4";
+import { Component5 } from "@/app/components/component5";
+import { Component6 } from "@/app/components/component6";
+import { Component10 } from "@/app/components/component10";
+import { CaseStudyCards } from "@/components/marketing/testimonials/case-study-cards";
+import { ContentDivider } from "@/components/application/content-divider/content-divider";
+
 export default function HomePage() {
     return (
         <div className="min-h-screen bg-primary">
+            {/* Component 1: Hero Section - Masjid & Madrasa Appeal */}
+            <Component1 />
+
+            <ContentDivider />
+
+            {/* Component 3: Community Messages & Duas */}
+            <Component3 />
+
+            {/* Component 4: Project Features (Masjid, Madrasa, etc.) */}
+            <Component4 />
+
+            <ContentDivider />
+
+            {/* Component 5: Masjid Feature Section */}
+            <Component5 />
+            <ContentDivider />
+
+            {/* Component 6: Madrasa Feature Section */}
+            <Component6 />
+            <ContentDivider />
+
+            {/* Case Study Cards: Testimonials Carousel */}
+            <CaseStudyCards />
+            <ContentDivider />
+
+            {/* Component 10: Final CTA & Donation Info */}
+            <Component10 />
         </div>
     );
 }
